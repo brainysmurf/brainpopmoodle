2723086bbc99eed8a28bb577ae262e99c5a2c616
Anthony Kuske <anthonykuske@gmail.com>
2014-11-26 19:18:51 +0000

    Update login url

--- locallib.php
+++ locallib.php
@@ -47,7 +47,7 @@ function brainpop_appears_valid_url($url)
 }
 
 function brainpop_get_domain($url)
-{ 
+{
 	$url = parse_url($url);
 	return $url['host'];
 }
@@ -75,7 +75,7 @@ function brainpop_fix_submitted_url($url)
         // please note relative urls are not allowed, /xx/yy links are ok
         $url = 'http://'.$url;
     }
-    
+
     return $url;
 }
 
@@ -85,13 +85,13 @@ function brainpop_fix_submitted_url($url)
 function brainpop_get_login_url( $item )
 {
 	$config = get_config('brainpop');
-	
+
 	$u = $config->brainpopusername;
 	$p = $config->brainpoppassword;
-	
+
 	$domain = brainpop_get_domain($item->externalurl);
-	
-	$url = 'http://'.$domain.'/user/loginDo.weml?user='.$u.'&password='.$p.'&targetPage=';
+
+	$url = 'https://'.$domain.'/user/loginDo.weml?uername='.$u.'&password='.$p.'&refer=';
 		$url .= urlencode($item->externalurl);
 
 	return $url;
@@ -150,11 +150,11 @@ function brainpop_get_full_url($item, $cm, $course, $config=null) {
         }
 
         if (!empty($parameters))
-        {    
+        {
 	        $join = (strpos($fullurl, '?') === false) ? '?' : '&';
 	        $fullurl = $fullurl.$join.implode('&', $parameters);
         }
-        
+
     }
 
     // encode all & to &amp; entity
@@ -238,7 +238,7 @@ function brainpop_print_intro($item, $cm, $course, $ignoresettings=false) {
 function brainpop_display_frame($item, $cm, $course)
 {
     global $PAGE, $OUTPUT, $CFG;
-    
+
 	brainpop_print_header($item, $cm, $course);
 	brainpop_print_heading($item, $cm, $course, true);
 	brainpop_print_intro($item, $cm, $course, true);
@@ -255,10 +255,10 @@ function brainpop_display_frame($item, $cm, $course)
 	echo '</div>';
 
     echo $OUTPUT->footer();
-    die;    
-    
-} 
- 
+    die;
+
+}
+
 /*function brainpop_display_frame($item, $cm, $course)
 {
     global $PAGE, $OUTPUT, $CFG;
